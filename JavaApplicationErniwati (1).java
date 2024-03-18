a4609bfc9577dafd0be486764c11485f5dec50fa
Erniwatibuulolo <163888252+Erniwatibuulolo@users.noreply.github.com>
2024-03-18 23:40:39 +0700

    Add files via upload

---
+++ JavaApplicationErniwati (1).java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication Erniwati Buulolo;
+
+/**
+ *
+ * @author Ogan
+ */
+public class JavaApplicationErniwati {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Datapribadi");
+        System.out.println("Nim:22100028");
+        System.out.println("Nama:Erniwati Buulolo");
+        System.out.println("J.K:Perempuan");
+        System.out.println("No/Wa:085261224556");
+        System.out.println("Email:buuloloerniwati@gmail.com");
+    }
+}
